0f4724df8dd609fa6f7ee844a2b4d05cdd18323f
silvia_PY <silvia_PY>
2017-02-05 20:15:57 +0800

    Initial commit

---
+++ library/src/main/java/com/example/library/StringUtils.java
@@ -0,0 +1,17 @@
+package com.example.library;
+
+public class StringUtils {
+	public final static String UTF_8 = "utf-8";
+
+	/** 判断字符串是否有值，如果为null或者是空字符串或者只有空格或者为"null"字符串，则返回true，否则则返回false */
+	public static boolean isEmpty(String value) {
+		if (value != null && !"".equalsIgnoreCase(value.trim())
+				&& !"null".equalsIgnoreCase(value.trim())) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+
+}
